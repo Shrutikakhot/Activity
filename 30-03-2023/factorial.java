d60291d7f50dfeebbec0080df4d4e0165e9d4665
Shrutikakhot <101885694+Shrutikakhot@users.noreply.github.com>
2023-04-17 22:30:40 +0530

    Create factorial.java

---
+++ 30-03-2023/factorial.java
@@ -0,0 +1,17 @@
+package practiceCode;
+
+public class factorial 
+{
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int i,fact=1;
+		System.out.println("Factorial from 1 to 10 : ");
+		for(i=1;i<=10;i++)
+		{
+			fact=i*fact;
+			
+		System.out.println(fact);
+		}	
+	}
+}
