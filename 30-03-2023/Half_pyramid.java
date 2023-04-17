f86870fd419b1ee76862a64d64f49cfb3e157d92
Shrutikakhot <101885694+Shrutikakhot@users.noreply.github.com>
2023-04-17 22:30:07 +0530

    Create Half_pyramid.java

---
+++ 30-03-2023/Half_pyramid.java
@@ -0,0 +1,18 @@
+package practiceCode;
+
+public class Half_pyramid 
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Half Pyramid");
+		for(int i=1; i<=4; i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}	
+	}
+}
